b5428e9e3f5a76d83a9dda9b0cce7c6ed68c1f6e
nicoconti1901 <nico.conti.1901@gmail.com>
2022-10-26 11:37:00 -0300

    Arreglo general de Front para modulo 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeadComponent } from './components/head/head.component';
+import { MainComponent } from './components/main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeadComponent
+    HeadComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
