08df46c0257103ab9660cfffd6a79f2a9961ae44
nicoconti1901 <nico.conti.1901@gmail.com>
2022-11-11 08:47:26 -0300

    Nav creacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeadComponent } from './components/head/head.component';
 import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LogoComponent } from './components/nav/logo/logo.component';
+import { SocialComponent } from './components/nav/social/social.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -12,10 +16,14 @@ import { MainComponent } from './components/main/main.component';
     AppComponent,
     HeaderComponent,
     HeadComponent,
-    MainComponent
+    MainComponent,
+    NavComponent,
+    LogoComponent,
+    SocialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
